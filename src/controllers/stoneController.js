239f1f0dafa31da2beda02049e252c9e406c50ce
0krasimira <kstoyanova2@gmail.com>
2024-02-18 17:27:12 +0200

    showed dynamic details page

--- src/controllers/stoneController.js
+++ src/controllers/stoneController.js
@@ -29,5 +29,18 @@ router.post('/create', isAuth, async(req, res) => {
        }
     })
 
+    router.get('/:stoneId/details', async (req, res) => {
+        const stoneId = req.params.stoneId
+        try{
+            const stone = await stoneManager.getOneWithDetails(stoneId).lean()
+            const isOwner = stone.owner._id.toString() == req.user?._id.toString()//movie.owner(object) ==  req.user._id(string) (convirts them to the same type)
+            // const casts = await castManager.getByIds(movie.casts).lean() //--- only if populate is not used(populates the cast info into the movie with the ref: Cast in the Movie Schema)
+            res.render('stone/details', {stone, isOwner})
+    } catch(error){
+        res.status(400).redirect('/404')
+    }
+     
+    })
+
 
 module.exports = router
